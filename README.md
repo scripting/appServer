04331e5114ac75f80620c21de8edef7f919225f7
Dave Winer <dave.winer@gmail.com>
2021-01-27 12:00:38 -0500

    v0.4.8

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
+# appserver
 
+A new release of nodeStorage that removes all the historical addons, streamlines configuration, only serves from the local file system, and serves the home page of the app. It could be thought of as v2.0 of nodeStorage except it is not backward compatible. 
+
+### Overview
+
+Over the years nodeStorage got a lot of appendages and add-ons that most apps don't use. 
+
+The configuration file was confusing and not well documented.
+
+It was one of the first things I wrote for Node, and I learned a lot over time.
+
+FInally when one of my programming partners had difficulty understanding the code, I realizaed I did too, and decided to rebuild it from scratch using components I had already developed, such as davehttp, davetwitter, etc. 
+
+I have converted Little Outliner to use this backend. The app was only modified in how it's configured. Everything else remains the same. It uses the same api glue file that it used to access nodeStorage. 
+
+### Configuring
+
+Below is an example of config.json file that goes in the same directory as the appserver.js file.
+
+### Example
+
+```json
+
+{
+
+"productName": "littleOutliner",
+
+"productNameForDisplay": "Little Outliner",
+
+"urlServerHomePageSource": "http://littleoutliner.com/testing/1.9.0/index.html",
+
+"prefsPath": "outlinerPrefs.json",
+
+"docsPath": "myOutlines/",
+
+
+
+"port": 1962,
+
+"flWebsocketEnabled": true,
+
+"websocketPort": 1963,
+
+"myDomain": "test.littleoutliner.com",
+
+
+
+"twitterConsumerKey": "7p1m9idcsyhkyoqmgsczibzbl",
+
+"twitterConsumerSecret": "clxqomjdm5983fv8dipzzhomshcosi2j3jp6hpkdpegnng3lc9"
+
+}
+
+```
+
+### Explanation
+
+The first section provides configuration information about and to the app that's running on the server's home page.  
+
+1. productName is an id that can be used to identify the product. No specified use.
+
+2. productNameForDisplay is what the app should use in its user interface.
+
+3. urlServerHomePageSource is the address of an HTML page that is served through the home page of the server. It can contain macros that plug in values from the server. 
+
+4. prefsPath is the name of the prefs file for the app. This is the file you should read and save to, to maintain the state of the app for the user. 
+
+5. docsPath is where the documents for this app are stored in the user's storage on the server.
+
+6. urlServerForClient is the address the app uses to call back to the server. 
+
+7. urlWebsocketServerForClient is the address of the web socket that reports on changes to files stored on the server. This address can be included in the app files so that apps that read the files can subbscribe to changes. 
+
+The second section configures the HTTP server and the connection to Twitter for identity. 
+
+1. port is the port the HTTP server will run on, unless process.env.PORT is specified. It overrides the port choice in the config file.
+
+2. flWebsocketEnabled, a boolean, determines whether or not we initialize the websocket server, if it's true then websocketPort is the port it's listening on. 
+
+3. myDomain, the domain assigned to this app. It's the domain you'd use to access the home page. If that includes a port, include the port in this value.
+
+4. twitterConsumerKey and twitterConsumerSecret are the values that identify the app for Twitter, as assigned on developer.twitter.com. 
 
--- appserver.js
+++ appserver.js
@@ -1,4 +1,4 @@
-var myVersion = "0.4.5", myProductName = "daveAppServer";
+var myVersion = "0.4.8", myProductName = "daveAppServer";
 
 exports.start = startup; 
 exports.notifySocketSubscribers = notifySocketSubscribers;
@@ -6,23 +6,37 @@ exports.saveStats = saveStats;
 exports.getConfig = getConfig;
 
 const fs = require ("fs");
+var dns = require ("dns");
+var os = require ("os");
 const request = require ("request");
 const websocket = require ("nodejs-websocket"); 
 const utils = require ("daveutils");
 const davehttp = require ("davehttp");
 const davetwitter = require ("davetwitter");
+const filesystem = require ("davefilesystem"); 
 
 const whenStart = new Date ();
 
 var config = {
+	productName: "randomApp",
+	productNameForDisplay: "Random App",
+	prefsPath: "prefs.json",
+	docsPath: "myDocs/",
 	flLogToConsole: true,
 	port: process.env.PORT || 1420,
 	websocketPort: 1422,
 	flAllowAccessFromAnywhere: true,
+	flPostEnabled: true, //12/21/20 by DW
 	flWebsocketEnabled: true,
 	urlServerForClient: "http://tagserver.opml.org/",
 	urlWebsocketServerForClient: "ws://tagserver.opml.org:1422/",
 	flEnableLogin: true, //user can log in via twitter
+	blockedAddresses: [], 
+	flForceTwitterLogin: true,
+	
+	flStorageEnabled: true, //1/23/21 by DW -- designed to work with nodeStorage installations as they are
+	privateFilesPath: "privateFiles/users/",
+	publicFilesPath: "publicFiles/users/"
 	};
 const fnameConfig = "config.json";
 
@@ -34,7 +48,6 @@ var stats = {
 	whenLastHit: new Date (0)
 	};
 const fnameStats = "stats.json";
-var theWsServer = undefined;
 
 function statsChanged () {
 	flStatsChanged = true;
@@ -67,53 +80,233 @@ function httpReadUrl (url, callback) {
 			}
 		});
 	}
-function notifySocketSubscribers (verb, jstruct) {
-	if (theWsServer !== undefined) {
-		var ctUpdates = 0, now = new Date (), jsontext = "";
-		if (jstruct !== undefined) { 
-			jsontext = utils.jsonStringify (jstruct);
+function checkPathForIllegalChars (path) {
+	function isIllegal (ch) {
+		if (utils.isAlpha (ch) || utils.isNumeric (ch)) {
+			return (false);
 			}
-		for (var i = 0; i < theWsServer.connections.length; i++) {
-			var conn = theWsServer.connections [i];
-			if (conn.bingeworthyData !== undefined) { //it's one of ours
-				try {
-					conn.sendText (verb + "\r" + jsontext);
-					conn.bingeworthyData.whenLastUpdate = now;
-					conn.bingeworthyData.ctUpdates++;
-					ctUpdates++;
-					}
-				catch (err) {
-					console.log ("notifySocketSubscribers: socket #" + i + ": error updating");
-					}
-				}
+		switch (ch) {
+			case "/": case "_": case "-": case ".":  case " ": case "*":
+				return (false);
 			}
-		if (ctUpdates > 0) {
-			console.log ("\nnotifySocketSubscribers: " + ctUpdates + " sockets were updated.\n");
+		return (true);
+		}
+	for (var i = 0; i < path.length; i++) {
+		if (isIllegal (path [i])) {
+			return (false);
 			}
 		}
+	if (utils.stringContains (path, "./")) {
+		return (false);
+		}
+	return (true);
 	}
-function webSocketStartup () {
-	if (config.flWebsocketEnabled) {
-		try {
-			theWsServer = websocket.createServer (function (conn) {
-				conn.bingeworthyData = {
-					whenLastUpdate: new Date (0),
-					ctUpdates: 0
-					};
-				});
-			theWsServer.listen (config.websocketPort);
+function getDomainName (clientIp, callback) { //11/14/15 by DW
+	if (clientIp === undefined) {
+		if (callback !== undefined) {
+			callback ("undefined");
 			}
-		catch (err) {
-			console.log ("webSocketStartup: err.message == " + err.message);
-			}
+		}
+	else {
+		dns.reverse (clientIp, function (err, domains) {
+			var name = clientIp;
+			if (!err) {
+				if (domains.length > 0) {
+					name = domains [0];
+					}
+				}
+			if (callback !== undefined) {
+				callback (name);
+				}
+			});
 		}
 	}
 
+//sockets
+	var theWsServer = undefined;
+	
+	function notifySocketSubscribers (verb, jstruct) {
+		if (theWsServer !== undefined) {
+			var ctUpdates = 0, now = new Date (), jsontext = "";
+			if (jstruct !== undefined) { 
+				jsontext = utils.jsonStringify (jstruct);
+				}
+			theWsServer.connections.forEach (function (conn, ix) {
+				if (conn.appData !== undefined) { //it's one of ours
+					try {
+						conn.sendText (verb + "\r" + jsontext);
+						conn.appData.whenLastUpdate = now;
+						conn.appData.ctUpdates++;
+						ctUpdates++;
+						}
+					catch (err) {
+						console.log ("notifySocketSubscribers: socket #" + i + ": error updating");
+						}
+					}
+				});
+			if (ctUpdates > 0) {
+				console.log ("\nnotifySocketSubscribers: " + ctUpdates + " sockets were updated.\n");
+				}
+			}
+		}
+	function checkWebSocketCalls () { //expire timed-out calls
+		}
+	function countOpenSockets () {
+		if (theWsServer === undefined) { //12/18/15 by DW
+			return (0);
+			}
+		else {
+			return (theWsServer.connections.length);
+			}
+		}
+	function getOpenSocketsArray () { //return an array with data about open sockets
+		var theArray = new Array ();
+		theWsServer.connections.forEach (function (conn, ix) {
+			if (conn.appData !== undefined) { //it's one of ours
+				theArray.push ({
+					arrayIndex: ix,
+					lastVerb: conn.appData.lastVerb,
+					urlToWatch: conn.appData.urlToWatch,
+					domain: conn.appData.domain,
+					whenStarted: utils.viewDate (conn.appData.whenStarted),
+					whenLastUpdate: utils.viewDate (conn.appData.whenLastUpdate)
+					});
+				}
+			});
+		return (theArray);
+		}
+	function handleWebSocketConnection (conn) { 
+		var now = new Date ();
+		conn.appData = { //initialize
+			whenStarted: now,
+			ctUpdates: 0,
+			whenLastUpdate: new Date (0),
+			lastVerb: undefined,
+			urlToWatch: undefined,
+			domain: undefined
+			};
+		
+		function logToConsole (conn, verb, value) {
+			getDomainName (conn.socket.remoteAddress, function (theName) { //log the request
+				var freemem = utils.gigabyteString (os.freemem ()), method = "WS:" + verb, now = new Date (); 
+				if (theName === undefined) {
+					theName = conn.socket.remoteAddress;
+					}
+				console.log (now.toLocaleTimeString () + " " + freemem + " " + method + " " + value + " " + theName);
+				conn.appData.domain = theName; 
+				});
+			}
+		
+		conn.on ("text", function (s) {
+			var words = s.split (" ");
+			if (words.length > 1) { //new protocol as of 11/29/15 by DW
+				conn.appData.whenLastUpdate = now;
+				conn.appData.lastVerb = words [0];
+				switch (words [0]) {
+					case "watch":
+						conn.appData.urlToWatch = utils.trimWhitespace (words [1]);
+						logToConsole (conn, conn.appData.lastVerb, conn.appData.urlToWatch);
+						break;
+					}
+				}
+			else {
+				conn.close ();
+				}
+			});
+		conn.on ("close", function () {
+			});
+		conn.on ("error", function (err) {
+			});
+		}
+	function webSocketStartup () {
+		if (config.flWebsocketEnabled) {
+			try {
+				theWsServer = websocket.createServer (handleWebSocketConnection);
+				console.log ("webSocketStartup: config.websocketPort == " + config.websocketPort);
+				theWsServer.listen (config.websocketPort);
+				}
+			catch (err) {
+				console.log ("webSocketStartup: err.message == " + err.message);
+				}
+			}
+		}
+//storage functions
+	function getFilePath (screenname, relpath, flprivate) {
+		const folder = (flprivate) ? config.privateFilesPath : config.publicFilesPath;
+		const f = folder + screenname + "/" + relpath;
+		return (f);
+		}
+	function publishFile (screenname, relpath, type, flprivate, filetext, callback) {
+		if (config.flStorageEnabled) {
+			var f = getFilePath (screenname, relpath, flprivate);
+			utils.sureFilePath (f, function () {
+				var now = new Date ();
+				fs.writeFile (f, filetext, function (err) {
+					if (err) {
+						callback (err);
+						}
+					else {
+						var url = (flprivate) ? undefined : config.urlServerForClient + screenname + "/" + relpath;
+						if (!flprivate) {
+							notifySocketSubscribers ("update", filetext);
+							}
+						callback (undefined, {
+							url,
+							whenLastUpdate: now
+							});
+						}
+					});
+				});
+			}
+		else {
+			callback ({message: "Can't publish the file because the feature is not enabled on the server."});
+			}
+		}
+	function getFile (screenname, relpath, flprivate, callback) {
+		if (config.flStorageEnabled) {
+			var f = getFilePath (screenname, relpath, flprivate);
+			fs.readFile (f, function (err, filetext) {
+				if (err) {
+					callback (err);
+					}
+				else {
+					callback (undefined, {filedata: filetext.toString ()});
+					}
+				});
+			}
+		else {
+			callback ({message: "Can't publish the file because the feature is not enabled on the server."});
+			}
+		}
+	function getFileList (screenname, flprivate, callback) {
+		var folder = getFilePath (screenname, "", flprivate);
+		filesystem.getFolderInfo (folder, function (theList) {
+			var returnedList = new Array ();
+			theList.forEach (function (item) {
+				var fname = utils.stringLastField (item.f, "/");
+				if (fname != ".DS_Store") {
+					returnedList.push ({
+						path: utils.stringDelete (item.f, 1, folder.length),
+						whenLastChange: item.whenModified,
+						whenCreated: item.whenCreated,
+						ctChars: item.size
+						});
+					}
+				});
+			console.log ("getFileList: folder == " + folder + ", returnedList == " + utils.jsonStringify (returnedList));
+			if (callback != undefined) {
+				callback (undefined, returnedList);
+				}
+			});
+		}
+
 function startup (options, callback) {
-	function readConfig (f, theConfig, callback) { 
+	function readConfig (f, theConfig, flReportError, callback) { 
 		fs.readFile (f, function (err, jsontext) {
 			if (err) {
-				console.log ("readConfig: err.message == " + err.message);
+				if (flReportError) { //1/21/21 by DW
+					console.log ("readConfig: err.message == " + err.message);
+					}
 				}
 			else {
 				try {
@@ -129,9 +322,28 @@ function startup (options, callback) {
 			callback ();
 			});
 		}
+	function startDavetwitter (httpRequestCallback) { //patch over a design problem in starting up davetwitter and davehttp -- 7/20/20 by DW 
+		if (config.twitter === undefined) {
+			config.twitter = new Object ();
+			}
+		config.twitter.myPort = config.port;
+		config.twitter.httpPort = config.port;
+		config.twitter.myDomain = config.myDomain;
+		config.twitter.flLogToConsole = config.flLogToConsole;
+		config.twitter.flAllowAccessFromAnywhere = config.flAllowAccessFromAnywhere;
+		config.twitter.flPostEnabled = config.flPostEnabled;
+		config.twitter.blockedAddresses = config.blockedAddresses;
+		config.twitter.httpRequestCallback = httpRequestCallback;
+		config.twitter.http404Callback = http404Callback; //1/24/21 by DW
+		config.twitter.twitterConsumerKey = config.twitterConsumerKey;
+		config.twitter.twitterConsumerSecret = config.twitterConsumerSecret;
+		davetwitter.start (config.twitter);
+		}
 	function handleHttpRequest (theRequest) {
 		const params = theRequest.params;
-		const token = params.oauth_token, secret = params.oauth_token_secret;
+		const token = params.oauth_token;
+		const secret = params.oauth_token_secret;
+		const flprivate = (params.flprivate === undefined) ? false : utils.getBoolean (params.flprivate);
 		
 		stats.ctHits++;
 		stats.ctHitsToday++;
@@ -160,6 +372,14 @@ function startup (options, callback) {
 		function returnError (jstruct) {
 			theRequest.httpReturn (500, "application/json", utils.jsonStringify (jstruct));
 			}
+		function httpReturn (err, jstruct) {
+			if (err) {
+				returnError (err);
+				}
+			else {
+				returnData (jstruct);
+				}
+			}
 		function httpReturnObject (err, jstruct) {
 			if (err) {
 				returnError (err);
@@ -169,63 +389,142 @@ function startup (options, callback) {
 				}
 			}
 		function returnServerHomePage () {
-			request (options.urlServerHomePageSource, function (error, response, templatetext) {
+			request (config.urlServerHomePageSource, function (error, response, templatetext) {
 				if (!error && response.statusCode == 200) {
 					var pagetable = {
-						productName: options.productName, 
-						productNameForDisplay: options.productNameForDisplay, 
-						version: options.version,
+						productName: config.productName, 
+						productNameForDisplay: config.productNameForDisplay, 
+						version: config.version,
 						urlServerForClient: config.urlServerForClient,
 						urlWebsocketServerForClient: config.urlWebsocketServerForClient,
-						flEnableLogin: config.flEnableLogin
+						flEnableLogin: config.flEnableLogin,
+						prefsPath: config.prefsPath,
+						docsPath: config.docsPath
 						};
+					if (config.addMacroToPagetable !== undefined) {
+						config.addMacroToPagetable (pagetable);
+						}
 					var pagetext = utils.multipleReplaceAll (templatetext, pagetable, false, "[%", "%]");
 					returnHtml (pagetext);
 					}
 				});
 			}
+		function callWithScreenname (callback) {
+			davetwitter.getScreenName (token, secret, function (screenname) {
+				if (screenname === undefined) {
+					returnError ({message: "Can't do the thing you want because the accessToken is not valid."});    
+					}
+				else {
+					callback (screenname);
+					}
+				});
+			}
 		
-		if (options.httpRequest !== undefined) {
-			if (options.httpRequest (theRequest)) { //consumed by callback
+		
+		if (config.httpRequest !== undefined) {
+			if (config.httpRequest (theRequest)) { //consumed by callback
 				return (true);
 				}
 			}
-		switch (theRequest.lowerpath) {
-			case "/":
-				returnServerHomePage ();
-				return (true);
-			case "/now":
-				returnPlainText (new Date ());
-				return (true);
-			case "/version":
-				returnData ({
-					productName: options.productName,
-					version: options.version
-					});
-				return (true);
-			case "/stats":
-				returnData (stats);
-				return (true);
+		switch (theRequest.lowermethod) {
+			case "post":
+				switch (theRequest.lowerpath) {
+					case "/publishfile": //1/22/21 by DW
+						callWithScreenname (function (screenname) {
+							publishFile (screenname, params.relpath, params.type, flprivate, theRequest.postBody.toString (), function (err, data) {
+								if (err) {
+									returnError (err);
+									}
+								else { //quirk in API, it wants a string, not a JSON struct
+									returnPlainText (utils.jsonStringify (data));
+									}
+								});
+							});
+						return (true);
+					}
+				break;
+			case "get":
+				switch (theRequest.lowerpath) {
+					case "/":
+						returnServerHomePage ();
+						return (true);
+					case "/now":
+						returnPlainText (new Date ());
+						return (true);
+					case "/version":
+						returnData ({
+							productName: config.productName,
+							version: config.version
+							});
+						return (true);
+					case "/stats":
+						returnData (stats);
+						return (true);
+					case "/getfile":
+						callWithScreenname (function (screenname) {
+							getFile (screenname, params.relpath, flprivate, httpReturn);
+							});
+						return (true); 
+					case "/getoptionalfile": 
+						callWithScreenname (function (screenname) {
+							getFile (screenname, params.relpath, flprivate, function (err, data) {
+								if (err) {
+									returnData ({}); //return nothing
+									}
+								else {
+									returnData (data);
+									}
+								});
+							});
+						return (true); 
+					case "/getfilelist": 
+						callWithScreenname (function (screenname) {
+							getFileList (screenname, flprivate, httpReturn);
+							});
+						return (true); 
+					}
+				break;
 			}
 		return (false);
 		}
-	function startDavetwitter (httpRequestCallback) { //patch over a design problem in starting up davetwitter and davehttp -- 7/20/20 by DW 
-		config.twitter.myPort = config.port;
-		config.twitter.httpPort = config.port;
-		config.twitter.flLogToConsole = config.flLogToConsole;
-		config.twitter.flAllowAccessFromAnywhere = config.flAllowAccessFromAnywhere;
-		config.twitter.flPostEnabled = config.flPostEnabled;
-		config.twitter.blockedAddresses = config.blockedAddresses;
-		config.twitter.httpRequestCallback = httpRequestCallback;
-		davetwitter.start (config.twitter);
+	function http404Callback (theRequest) {
+		if (config.flStorageEnabled) {
+			if (checkPathForIllegalChars (theRequest.path)) {
+				function return404 () {
+					theRequest.httpReturn (404, "text/plain", "Not found.");
+					}
+				function returnPlainText (s) {
+					theRequest.httpReturn (200, "text/plain", s.toString ());
+					}
+				var path = utils.stringDelete (theRequest.path, 1, 1); //delete leading slash
+				var screenname = utils.stringNthField (path, "/", 1);
+				var relpath = utils.stringDelete (path, 1, screenname.length + 1);
+				var flprivate = false;
+				getFile (screenname, relpath, flprivate, function (err, data) {
+					if (err) {
+						return404 ();
+						}
+					else {
+						returnPlainText (data.filedata);
+						}
+					});
+				}
+			else {
+				return404 ();
+				}
+			return (true); //tell davetwitter we handled it
+			}
+		else {
+			return (false); //tell davetwitter we didn't handle it
+			}
 		}
 	function everyMinute () {
 		var now = new Date ();
-		if (options.everyMinute !== undefined) {
-			options.everyMinute ();
+		if (config.everyMinute !== undefined) {
+			config.everyMinute ();
 			}
 		if (now.getMinutes () == 0) {
-			console.log ("\n" + now.toLocaleTimeString () + ": " + options.productName + " v" + options.version + " running on port " + config.port + ".\n");
+			console.log ("\n" + now.toLocaleTimeString () + ": " + config.productName + " v" + config.version + " running on port " + config.port + ".\n");
 			}
 		}
 	function everySecond () {
@@ -235,12 +534,11 @@ function startup (options, callback) {
 			fs.writeFile (fnameStats, utils.jsonStringify (stats), function () {
 				});
 			}
-		if (options.everySecond !== undefined) {
-			options.everySecond ();
-			}
 		}
-	readConfig (fnameConfig, config, function () {
-		readConfig (fnameStats, stats, function () {
+	
+	utils.copyScalars (options, config); //1/22/21 by DW
+	readConfig (fnameConfig, config, true, function () { //anything can be overridden by config.json
+		readConfig (fnameStats, stats, false, function () {
 			if (process.env.PORT !== undefined) { //8/6/20 by DW
 				config.port = process.env.PORT;
 				}
@@ -248,14 +546,21 @@ function startup (options, callback) {
 			stats.ctHitsThisRun = 0;
 			stats.whenLastStart = whenStart;
 			statsChanged ();
-			console.log ("\n" + options.productName + " v" + options.version + " running on port " + config.port + ".\n");
+			console.log ("\n" + config.productName + " v" + config.version + " running on port " + config.port + ".\n");
 			console.log ("config == " + utils.jsonStringify (config)); 
 			startDavetwitter (handleHttpRequest);
-			webSocketStartup (); 
-			setInterval (everySecond, 1000); 
-			utils.runEveryMinute (everyMinute); 
-			if (callback !== undefined) {
-				callback (config);
+			if (config.myDomain === undefined) {
+				console.log ("startup: can't start the server because config.myDomain is not defined.");
+				}
+			else {
+				config.urlServerForClient = "http://" + config.myDomain + "/";
+				config.urlWebsocketServerForClient = "ws://" + utils.stringNthField (config.myDomain, ":", 1) + ":" + config.websocketPort + "/";
+				webSocketStartup (); 
+				setInterval (everySecond, 1000); 
+				utils.runEveryMinute (everyMinute); 
+				if (callback !== undefined) {
+					callback (config);
+					}
 				}
 			});
 		});
